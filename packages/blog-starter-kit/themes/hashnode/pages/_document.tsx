93590140176541458739b848b6b6ea4adc272f83
Krrish Bhurani <90558815+lil0xplorer@users.noreply.github.com>
2025-07-28 13:58:22 +0530

    Update _document.tsx

--- packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
@@ -1,9 +1,19 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				{/* Optional: Google Site Verification Meta Tag (replace with your actual code if needed) */}
+				<meta name="google-adsense-account" content="ca-pub-8155500837029256" />
+
+				{/* Google AdSense Script */}
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8155500837029256"
+					crossOrigin="anonymous"
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
